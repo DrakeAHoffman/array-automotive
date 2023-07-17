0c2a72aacb9767c4865d6c9e8eab75f65cd8026a
DrakeAHoffman <drakehoffman99@gmail.com>
2023-07-16 18:16:22 -0700

    created-new-index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+
+
+const VehicleModule = require('./vehicle').Vehicle
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+//above I added the max passengers, the intitial amount of passengers, # of wheels, the max speed, fuel, & the schedule service
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+// above I input all the methods from the activity, including name, parameters, & logic
+}
+
+let myCar = new Car('Ford', 'F-150', '2014', 'silver', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
+
+//I finished off by using my own vehicle lol although it tops out at 100...
